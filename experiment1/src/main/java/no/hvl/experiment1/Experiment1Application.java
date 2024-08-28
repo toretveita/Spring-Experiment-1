3d79159570270e534d2e75e24c3be46541fef4ca
toretveita <69637759+toretveita@users.noreply.github.com>
2024-08-28 22:19:03 +0200

    Add files via upload

---
+++ experiment1/src/main/java/no/hvl/experiment1/Experiment1Application.java
@@ -0,0 +1,13 @@
+package no.hvl.experiment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Experiment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Experiment1Application.class, args);
+	}
+
+}
---
+++ experiment1/src/test/java/no/hvl/experiment1/Experiment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.experiment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Experiment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
